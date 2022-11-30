6223e2e8ab06449e942c163d1d0d353741c74d4d
Chen Lin <chen.lin.0404@gmail.com>
2022-11-30 14:58:31 -0800

    update scripts after file path change

--- src/run_all_scripts.sh
+++ src/run_all_scripts.sh
@@ -1,15 +1,17 @@
 python data_download.py --url='https://info.stackoverflowsolutions.com/rs/719-EMH-566/images/stack-overflow-developer-survey-2022.zip'
- --out_file='data/raw'
+ --out_file='../data/raw'
  
-# Use raw data to output 3 files:
+# Use raw data to output 5 files:
 # 1. data_filtered.csv
 # 2. train.csv (90% from data_filtered.csv)
 # 3. test.csv (10% from data_filtered.csv)
+# 4. train_encoded.csv (encoded/preprocessed data from train.csv)
+# 5. test_encoded.csv (encoded/preprocessed data from test.csv)
 python data_preprocessing.py --data_input='../data/raw/survey_results_public.csv' --data_output_path='../data/processed/'
 
 # Use data_filtered.csv do EDA
 # Save generated files or images
-python data_eda.py --data_input='../data/processed/train.csv' -file_out_path='../documents/' 
+python data_eda.py --data_input='../data/processed/train_encoded.csv' -file_out_path='../documents/' 
 
 # Use train.csv do model selection, hyperparameter tuning, final model traning and scoring with test.csv
 # Save any file generated
